5ea7e906d64d9698359a7f2e3debaf23ce0c3414
Desmond Cain <desmondcain@gmail.com>
2015-03-25 13:55:55 -0500

    Add bridging header and set file path

---
+++ WeightCapBLE/WeightCapBLE-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  WeightCapBLE-Bridging-Header.h
+//  WeightCapBLE
+//
+//  Created by Desmond Cain on 3/25/15.
+//  Copyright (c) 2015 Desmond Cain. All rights reserved.
+//
+
+#import "MetaWear/MetaWear.h"
