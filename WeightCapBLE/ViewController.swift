a1bcf92b434761832454d40b75e90f6145f08517
Desmond Cain <desmondcain@gmail.com>
2015-06-07 14:01:08 -0500

    Add MW and Accel interface functions for v1.5.3

--- WeightCapBLE/ViewController.swift
+++ WeightCapBLE/ViewController.swift
@@ -9,17 +9,154 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let sharedManager = MBLMetaWearManager.sharedManager()
+    var device = MBLMetaWear.new()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        sharedManager.startScanForMetaWearsWithHandler() {
+            (deviceArray) -> Void in println("Detected MetaWear devices: \(deviceArray)")
+
+            self.sharedManager.stopScanForMetaWears()
+            
+            self.device = deviceArray[0] as! MBLMetaWear
+            
+            self.device.connectWithHandler() {
+                (error) -> Void in println("Connected to MetaWear: \(self.device)")
+                
+                self.flashLED()
+                
+                self.getBatteryLife()
+
+                self.getSerialNumber()
+                
+                self.getFirmwareRevision()
+                
+                self.checkForFirmwareUpdates()
+//
+//                self.getHardwareRevision()
+//
+//                self.getManufacturerName()
+                
+//                self.getAmbientTemp()
+                
+//                self.streamRMSData()
+            }
+            
+            let config = self.device.configuration
+            
+//            self.device.setConfiguration(<#configuration: MBLRestorable!#>, handler: <#MBLErrorHandler!##(NSError!) -> Void#>)
+            
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func streamRMSData() {
+        self.device.accelerometer.fullScaleRange = MBLAccelerometerRange(2)        
+        self.device.accelerometer.filterCutoffFreq = 0
+        self.device.accelerometer.highPassFilter = true
+//        self.device.accelerometer.lowNoise = true
+        self.device.accelerometer.sampleFrequency = MBLAccelerometerSampleFrequency(100)
+        
+//        self.device.accelerometer.dataReadyEvent.startNotificationsWithHandler() {
+//            (obj, error) -> Void in println("Raw Data: \(obj)")
+//        }
+        
+        // Sum the RMS data
+        let summedRMS = self.device.accelerometer.rmsDataReadyEvent.summationOfEvent()
+        
+        // Set logging interval
+        let periodicRMS = summedRMS.periodicSampleOfEvent(2000)
+        
+        periodicRMS.startNotificationsWithHandler() {
+            (obj, error) -> Void in println("RMS: \(obj)")
+        }
+        
+//        self.device.accelerometer.rmsDataReadyEvent.startNotificationsWithHandler() {
+//            (obj, error) -> Void in println("RMS: \(obj)")
+//        }
 
+        
+//        self.device.accelerometer.fullScaleRange = MBLAccelerometerRange8G;  // Default: +- 8G
+//        self.device.accelerometer.sampleFrequency = MBLAccelerometerSampleFrequency100 // Default: 100 Hz
+//        [device.accelerometer.dataReadyEvent startNotificationsWithHandler:^(MBLAccelerometerData *obj, NSError *error) {
+//            NSLog(@"X = %d, Y = %d, Z = %d", obj.x, obj.y, obj.z);
+//        }];
+    }
+    
+    func getBatteryLife() {
+        self.device.readBatteryLifeWithHandler() {
+            (number, error) -> Void in println("Battery: \(number)%")
+        }
+    }
+    
+    func getAmbientTemp() {
+        self.device.temperature.source = MBLTemperatureSource.Internal
+        self.device.temperature.units = MBLTemperatureUnit.Fahrenheit
+        self.device.temperature.readTemperatureWithHandler() {
+            (number, error) -> () in println("Temp: \(number)° F")
+        }
+    }
+    
+    func getSerialNumber() {
+        var serialNumber = self.device.deviceInfo.serialNumber
+        println("Serial #: \(serialNumber)")
+    }
+    
+    func getFirmwareRevision() {
+        var firmwareRevision = self.device.deviceInfo.firmwareRevision
+        println("Firmware revision: \(firmwareRevision)")
+    }
+    
+//    func updateFirmaware() {
+//        self.device.updateFirmwareWithHandler(<#handler: MBLErrorHandler!##(NSError!) -> Void#>, progressHandler: <#MBLFloatHandler!##(Float, NSError!) -> Void#>)
+//    }
+    
+    func checkForFirmwareUpdates() {
+        self.device.checkForFirmwareUpdateWithHandler { (result: Bool, error) -> Void in
+            if (result == true) {
+                println("Update required")
+            } else {
+                println("No updates found")
+            }
+        }
+    }
+    
+    func getHardwareRevision() {
+        var hardwareRevision = self.device.deviceInfo.hardwareRevision
+        println("Hardware revision: \(hardwareRevision)")
+    }
+    
+    func getManufacturerName() {
+        var manufacturerName = self.device.deviceInfo.manufacturerName
+        println("Manufacturer Name: \(manufacturerName)")
+    }
+    
+    func flashLED() {
+        self.device.led.flashLEDColor(UIColor.whiteColor(), withIntensity: 0.5, numberOfFlashes: 5)
+    }
+    
+    func disconnectDevice() {
+        self.device.disconnectWithHandler() {
+            (error) -> Void in println("Disconnected from MetaWear: \(self.device)")
+        }
+    }
+    
+    func rememberDevice() {
+        self.device.rememberDevice()
+    }
+    
+    func forgetDevice() {
+        self.device.forgetDevice()
+    }
 
 }
 
